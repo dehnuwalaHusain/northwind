42c8469cfd53350b3e64b96de8233b2af6be38a5
Jashaswimalya Acharjee <jashaswimalyaacharjee@gmail.com>
2023-06-09 16:23:11 +0530

    Seamless dataloading in MongoDB

--- mongodb-northwind/mongo-import.sh
+++ mongodb-northwind/mongo-import.sh
@@ -1,9 +1,11 @@
+cd /mongodb-northwind
+
 for file in json/*.json
 do
     filename=$(basename "$file")
     collection="${filename%.*}"
     
     echo $filename
-    mongoimport --host "127.0.0.1:27017" -u "root" -p "somestrongpassword" --authenticationDatabase "admin" -d "$1" -c "$collection" --type json --file "$file"
+    mongoimport --host "127.0.0.1:27017" -u "root" -p "somestrongpassword" --authenticationDatabase "admin" -d "northwind" -c "$collection" --type json --file "$file"
 done
 
